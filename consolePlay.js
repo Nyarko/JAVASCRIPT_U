ca623908dfd5cb4001bf4444f052d04f2010f2f3
Nyarko <nyarko.agyekum@yahoo.com>
2022-09-03 21:54:18 +0000

    not sure where i'm at, pc went off

--- consolePlay.js
+++ consolePlay.js
@@ -2,8 +2,12 @@ console.log('Hello dear Friends')
 console.log('\n')
 
 /* Next up! */
+console.log('This is a for loop:')
 for (var i = 0; i < 10; i++) {
     console.log(i);
 }
 
 console.log('\n')
+console.log(3 + 2.1 )
+
+console.log(3 > 2.1 )
