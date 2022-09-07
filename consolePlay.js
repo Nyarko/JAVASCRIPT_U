650519e6f7118d69888c9b2a62f709daaa6a778a
Nyarko <nyarko.agyekum@yahoo.com>
2022-09-07 10:36:38 +0000

    Code refactored, removed unnecessary lines

--- consolePlay.js
+++ consolePlay.js
@@ -59,85 +59,29 @@ console.log('\n')
 
 
 //EVEN OR ODD MODIFIED
-function numberChecker() {
-    const interact = require("readline");
-
-    const rLII /*r=read L=Line I=Interface I = Instance */ = interact.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
-
-    rLII.question('Want to run the NumberChecker?\n', function (answer) {
-        if (answer == 'Yes' || answer == "yes" || answer == 'y'){
-            console.log('\n\tAlright!\nRunning the NumberChecker now:\n')
-            runNumberChecker();
-        } else {
-            console.log('Then closing Query instance')
-            rLII.close();
-        }
-    });
-
-    function runNumberChecker() {
-        
-        
-        rLII.question('***********************************\n'+
-                    '***********NumberChecker***********\n' + 
-                    '\nWhat number do you want to check?\n', 
-                    function (answer){
-            if (answer % 2 == 0) {
-                console.log('\n' + answer + ' is even')
-            } else {
-                console.log('\n' + answer + ' is odd')
-            }
-
-            rLII.question('\nWish to check a different number? ',
-                                function (answer) {
-                                    if (answer == 'Yes' || answer == "yes" || answer == 'y'){
-                                        console.log('\n\tOkay!\nRunning the NumberChecker again:\n')
-                                        runNumberChecker();
-                                    } else {
-                                        console.log('\nUnderstood!')
-                                        console.log("\nClosing the NumberChecker interface")
-                                        console.log('\n\tClosing Query interface')
-                                        console.log('\n************/*/Goodbye/*/***********\n')
-                                        console.log('************************************\n')
-                                        rLII.close()
-                                    }
-                                })
-        })
-    };
-}
-console.log(numberChecker());
-
-/*const userInput = prompt("Number to check for odd or even:");
+//HARDCODED
+const userInput = 10;
 if (userInput % 2 == 0) {
     console.log('This is even')
 } else {
     console.log('This is odd')
 }
-console.log('\n')*/
-
-//MUSICAL GROUPS
 console.log('\n')
 
-//INTERACTIVE:
-
+//MUSICAL GROUPS
 //HARDCODED:
-function hardMG () {
-    const musicians = 10;
+const musicians = 10;
 
-    if (musicians <= 0) {
-        console.log('Not a group')
-    } else if (musicians == 1) {
-        console.log('Solo')
-    } else if (musicians == 2) {
-        console.log('Duet')
-    } else if (musicians === 3) {
-        console.log("trio");
-    } else if (musicians === 4) {
-        console.log("quartet");
-    } else {
-        console.log("this is a large group");
-    }
+if (musicians <= 0) {
+    console.log('Not a group')
+} else if (musicians == 1) {
+    console.log('Solo')
+} else if (musicians == 2) {
+    console.log('Duet')
+} else if (musicians === 3) {
+    console.log("trio");
+} else if (musicians === 4) {
+    console.log("quartet");
+} else {
+    console.log("this is a large group");
 }
-console.log(hardMG());
