eaafee75221b52b43016e4f7da94b3625116af93
Nyarko <nyarko.agyekum@yahoo.com>
2022-09-07 09:33:46 +0000

    Refactored code: Put interactive NumberChecker in numberChecker function, removing code at line 119 as it gave error.

--- consolePlay.js
+++ consolePlay.js
@@ -57,54 +57,57 @@ const total = bill + tip;
 console.log('The total cost is $' + total.toFixed(2));
 console.log('\n')
 
+
 //EVEN OR ODD MODIFIED
-const interact = require("readline");
+function numberChecker() {
+    const interact = require("readline");
 
-const rLII /*r=read L=Line I=Interface I = Instance */ = interact.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+    const rLII /*r=read L=Line I=Interface I = Instance */ = interact.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
 
-rLII.question('Want to run the NumberChecker?\n', function (answer) {
-    if (answer == 'Yes' || answer == "yes" || answer == 'y'){
-        console.log('\n\tAlright!\nRunning the NumberChecker now:\n')
-        runNumberChecker();
-    } else {
-        console.log('Then closing Query instance')
-        rLII.close();
-    }
-});
-
-function runNumberChecker() {
-    
-    
-    rLII.question('***********************************\n'+
-                '***********NumberChecker***********\n' + 
-                '\nWhat number do you want to check?\n', 
-                function (answer){
-        if (answer % 2 == 0) {
-            console.log('\n' + answer + ' is even')
+    rLII.question('Want to run the NumberChecker?\n', function (answer) {
+        if (answer == 'Yes' || answer == "yes" || answer == 'y'){
+            console.log('\n\tAlright!\nRunning the NumberChecker now:\n')
+            runNumberChecker();
         } else {
-            console.log('\n' + answer + ' is odd')
+            console.log('Then closing Query instance')
+            rLII.close();
         }
+    });
 
-        rLII.question('\nWish to check a different number? ',
-                            function (answer) {
-                                if (answer == 'Yes' || answer == "yes" || answer == 'y'){
-                                    console.log('\n\tOkay!\nRunning the NumberChecker again:\n')
-                                    runNumberChecker();
-                                } else {
-                                    console.log('\nUnderstood!')
-                                    console.log("\nClosing the NumberChecker interface")
-                                    console.log('\n\tClosing Query interface')
-                                    console.log('\n************/*/Goodbye/*/***********\n')
-                                    console.log('************************************\n')
-                                    rLII.close()
-                                }
-                            })
-    })
-};
+    function runNumberChecker() {
+        
+        
+        rLII.question('***********************************\n'+
+                    '***********NumberChecker***********\n' + 
+                    '\nWhat number do you want to check?\n', 
+                    function (answer){
+            if (answer % 2 == 0) {
+                console.log('\n' + answer + ' is even')
+            } else {
+                console.log('\n' + answer + ' is odd')
+            }
 
+            rLII.question('\nWish to check a different number? ',
+                                function (answer) {
+                                    if (answer == 'Yes' || answer == "yes" || answer == 'y'){
+                                        console.log('\n\tOkay!\nRunning the NumberChecker again:\n')
+                                        runNumberChecker();
+                                    } else {
+                                        console.log('\nUnderstood!')
+                                        console.log("\nClosing the NumberChecker interface")
+                                        console.log('\n\tClosing Query interface')
+                                        console.log('\n************/*/Goodbye/*/***********\n')
+                                        console.log('************************************\n')
+                                        rLII.close()
+                                    }
+                                })
+        })
+    };
+}
+console.log(numberChecker());
 
 /*const userInput = prompt("Number to check for odd or even:");
 if (userInput % 2 == 0) {
@@ -113,7 +116,6 @@ if (userInput % 2 == 0) {
     console.log('This is odd')
 }
 console.log('\n')*/
-rLII.close();
 
 //MUSICAL GROUPS
 console.log('\n')
